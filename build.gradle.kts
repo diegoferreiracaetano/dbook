55570d7dab740e16bfc42a5da2669cb1436ca4db
diegoferreiracaetano <di.cafe@hotmail.com>
2026-09-07 21:31:49 -0300

    Setup inicial: projeto Spring Boot + Kotlin, wrapper e health check
    Item 1.1 (setup do projeto) e 1.2 (HealthController + git) do checklist
    SmartBook Flights.

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	kotlin("jvm") version "2.0.20"
+	kotlin("plugin.spring") version "2.0.20"
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.smartbook"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
