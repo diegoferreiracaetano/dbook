d824f6be531f945adbb96e509dc0c100defa3255
diegoferreiracaetano <di.cafe@hotmail.com>
2026-09-09 23:30:04 -0300

    Reduz o mínimo de cobertura do JaCoCo de 80% para 75%

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
             limit {
-                minimum = "0.80".toBigDecimal()
+                minimum = "0.75".toBigDecimal()
             }
         }
     }
